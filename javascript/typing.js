adda96a65d10bf2dc666ae2fb2e389d8696fa507
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-11-15 14:31:00 +0530

    select elements from html file into js

---
+++ javascript/typing.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', init);
+
+// Globals
+
+// Available Levels
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 1
+};
+
+// To change level
+const currentLevel = levels.medium;
+
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
